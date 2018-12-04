779d10b6c54ab5fce8abd5dd0bf28a2bc87183d2
makeabignews <makeabignews@sina.com>
2018-12-03 20:25:09 -0500

    up

--- 6.finishbuild.sh
+++ 6.finishbuild.sh
@@ -2,13 +2,13 @@
 genesisHash=$(cat ./genesisHash.txt)
 text=$(cat ./config-files/constants.js)
 text=${text/REPLACE_GENESIS_UNIT/$genesisHash}
-echo text > ./config-files/constants.js
+echo $text > ./config-files/constants.js
 
 witnessAddress=$(cat ./data/witnessAddress.json)
 text=$(cat ./config-files/hub-conf.js)
 text=${text/REPLACE_WITNESSES/$witnessAddress}
-echo text > ./config-files/hub-conf.js
+echo $text > ./config-files/hub-conf.js
 
 text=$(cat ./config-files/explorer-conf.js)
 text=${text/REPLACE_WITNESSES/$witnessAddress}
-echo text > ./config-files/explorer-conf.js
+echo $text > ./config-files/explorer-conf.js
