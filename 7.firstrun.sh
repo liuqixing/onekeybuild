532a9cbdf7d674604c68abf02f599ca9aaa0b791
makeabignews <makeabignews@sina.com>
2018-12-04 03:50:45 -0500

    up

--- 7.firstrun.sh
+++ 7.firstrun.sh
@@ -2,6 +2,7 @@
 ./deploy.sh
 ./start.sh
 rm -rf ~/.config/headless15/trustnote*
+cp ~/.config/trustnote-hub/trustnote.sqlite ~/.config/headless15/trustnote.sqlite
 cd ./trustnote-headless/play
 node create_genesis.js pay
 echo "finish all!"
