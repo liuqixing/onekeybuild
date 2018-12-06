42a6eee493c66645baa00d7fa6320951cfa1107c
makeabignews <makeabignews@sina.com>
2018-12-06 17:12:51 +0800

    up

--- 1.create_conf.sh
+++ 1.create_conf.sh
@@ -6,5 +6,6 @@ echo "取消2次手动输入"
 cp ./config-files/witness-headless-start.js ./trustnote-headless/play/node_modules/trustnote-headless/start.js
 cp ./config-files/witness-headless-start.js ./trustnote-headless/start.js
 cd ./trustnote-headless/play
+mkdir data
 node create_allConfig.js
 cd ../../
