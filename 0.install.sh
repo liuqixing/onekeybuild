23b0bd077c386e1f5e32bd2165f81b6d23c3177b
makeabignews <makeabignews@sina.com>
2018-12-06 02:44:28 -0500

    Update 0.install.sh

--- 0.install.sh
+++ 0.install.sh
@@ -1,8 +1,3 @@
-sudo apt-get install build-essential git jq tree ruby
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-nvm install 8.9.4
-sudo npm install pm2 -g
-sudo npm install node-gyp -g
 arr=(trustnote-hub trustnote-witness trustnote-headless trustnote-explorer)
 echo clone
 for name in ${arr[@]}
