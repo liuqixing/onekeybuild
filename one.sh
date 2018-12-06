cd59b5a3834ba6e630a9858d73ec0eb6466dbd99
makeabignews <makeabignews@sina.com>
2018-12-06 02:44:53 -0500

    Update one.sh

--- one.sh
+++ one.sh
@@ -1,5 +1,10 @@
-./remove-all-data.sh
-sudo killall -9 node
+sudo apt-get install build-essential git jq tree ruby
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+nvm install 8.9.4
+sudo npm install pm2 -g
+sudo npm install node-gyp -g
+git clone https://github.com/trustnote/onekeybuild.git
+cd onekeybuild
 ./0.install.sh
 ./1.create_conf.sh
 ./2.create_gen.sh
